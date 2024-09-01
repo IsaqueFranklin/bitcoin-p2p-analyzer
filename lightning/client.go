4aead47ed0bb66ec8b4da482b888bff46bb479e8
Isaque Franklin Amaral <isaquefrankli@gmail.com>
2024-09-01 20:58:02 -0300

    Shitty code.

--- lightning/client.go
+++ lightning/client.go
@@ -1,3 +1,5 @@
+// lightning/client.go
+
 package lightning
 
 import (
@@ -5,11 +7,13 @@ import (
 	"encoding/hex"
 	"fmt"
 	"io/ioutil"
+	"time"
 
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 	"gopkg.in/macaroon.v2"
 
 	"github.com/lncm/lnd-rpc/v0.10.0/lnrpc"
-	"github.com/lncm/lnd-rpc/v0.8.2/lnrpc"
 )
 
 type rpcCreds map[string]string
@@ -18,10 +22,9 @@ func (m rpcCreds) RequireTransportSecurity() bool { return true }
 func (m rpcCreds) GetRequestMetadata(_ context.Context, _ ...string) (map[string]string, error) {
 	return m, nil
 }
-
 func newCreds(bytes []byte) rpcCreds {
 	creds := make(map[string]string)
-	creds["macaron"] = hex.EncodeToString(bytes)
+	creds["macaroon"] = hex.EncodeToString(bytes)
 	return creds
 }
 
@@ -35,4 +38,25 @@ func getClient(hostname string, port int, tlsFile, macaroonFile string) lnrpc.Li
 	if err = mac.UnmarshalBinary(macaroonBytes); err != nil {
 		panic(fmt.Sprintln("Cannot unmarshal macaroon", err))
 	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	transportCredentials, err := credentials.NewClientTLSFromFile(tlsFile, hostname)
+	if err != nil {
+		panic(err)
+	}
+
+	fullHostname := fmt.Sprintf("%s:%d", hostname, port)
+
+	connection, err := grpc.DialContext(ctx, fullHostname, []grpc.DialOption{
+		grpc.WithBlock(),
+		grpc.WithTransportCredentials(transportCredentials),
+		grpc.WithPerRPCCredentials(newCreds(macaroonBytes)),
+	}...)
+	if err != nil {
+		panic(fmt.Errorf("unable to connect to %s: %w", fullHostname, err))
+	}
+
+	return lnrpc.NewLightningClient(connection)
 }
